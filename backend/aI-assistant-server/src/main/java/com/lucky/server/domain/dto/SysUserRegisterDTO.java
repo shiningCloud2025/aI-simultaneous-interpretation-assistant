d903502f9374216d6ba31c92e4ef867c3dbef5fa
shiningCloud2025 <shining_cloud2025@163.com>
2026-08-06 00:03:02 +0800

    fix:账号仅支持数字并补充注解

--- backend/aI-assistant-server/src/main/java/com/lucky/server/domain/dto/SysUserRegisterDTO.java
+++ backend/aI-assistant-server/src/main/java/com/lucky/server/domain/dto/SysUserRegisterDTO.java
@@ -3,6 +3,7 @@ package com.lucky.server.domain.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 /**
