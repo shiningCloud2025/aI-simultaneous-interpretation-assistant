eb14d87d41a7e7cb44e54be492bc1f1cc7f6c5e8
shiningCloud2025 <shining_cloud2025@163.com>
2026-08-06 23:26:02 +0800

    fix:修改快捷键的用户id占位

--- backend/aI-assistant-server/src/main/java/com/lucky/server/service/impl/SysUserShortcutConfigServiceImpl.java
+++ backend/aI-assistant-server/src/main/java/com/lucky/server/service/impl/SysUserShortcutConfigServiceImpl.java
@@ -7,6 +7,7 @@ import com.lucky.server.domain.dto.SysUserShortcutConfigSaveDTO.ShortcutItem;
 import com.lucky.server.domain.entity.SysUserShortcutConfig;
 import com.lucky.server.domain.vo.SysUserShortcutConfigVO;
 import com.lucky.server.mapper.SysUserShortcutConfigMapper;
+import com.lucky.server.service.SysUserService;
 import com.lucky.server.service.SysUserShortcutConfigService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -27,10 +28,10 @@ import java.util.stream.Collectors;
 @Service
 public class SysUserShortcutConfigServiceImpl extends ServiceImpl<SysUserShortcutConfigMapper, SysUserShortcutConfig> implements SysUserShortcutConfigService {
 
+    private SysUserService sysUserService;
     @Override
     public List<SysUserShortcutConfigVO> listByUserId() {
-        // TODO: 后续从 SecurityContext 获取当前用户ID
-        Long userId = null;
+        Long userId = sysUserService.getCurrentUser().getId();
         return lambdaQuery().eq(SysUserShortcutConfig::getUserId, userId).list()
                 .stream()
                 .map(e -> new SysUserShortcutConfigVO(e.getId(), e.getAction(), e.getKeyCombination()))
@@ -40,8 +41,7 @@ public class SysUserShortcutConfigServiceImpl extends ServiceImpl<SysUserShortcu
     @Override
     @Transactional(rollbackFor = Exception.class)
     public void batchSaveOrUpdate(List<ShortcutItem> dtos) {
-        // TODO: 后续从 SecurityContext 获取当前用户ID
-        Long userId = null;
+        Long userId = sysUserService.getCurrentUser().getId();
 
         List<SysUserShortcutConfig> existingList = lambdaQuery()
                 .eq(SysUserShortcutConfig::getUserId, userId).list();
