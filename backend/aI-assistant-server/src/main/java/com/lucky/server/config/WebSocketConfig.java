b49462a5525efcc46ab638c5ca616afccf2ca94f
shiningCloud2025 <shining_cloud2025@163.com>
2026-08-20 10:08:42 +0800

    后端websocket适配

--- backend/aI-assistant-server/src/main/java/com/lucky/server/config/WebSocketConfig.java
+++ backend/aI-assistant-server/src/main/java/com/lucky/server/config/WebSocketConfig.java
@@ -25,7 +25,7 @@ public class WebSocketConfig implements WebSocketConfigurer{
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(audioHandler,"/asia/audio")
+        registry.addHandler(audioHandler,"/asr/audio")
                 //  握手鉴权
                 .addInterceptors(authHandshakeInterceptor)
                 .setAllowedOrigins("*");
