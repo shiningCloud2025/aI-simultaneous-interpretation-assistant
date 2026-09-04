83d5119c8fcd048d05fa40946a5c04db639532e4
shiningCloud2025 <shining_cloud2025@163.com>
2026-09-04 09:32:27 +0800

    口语业务-枚举准备

---
+++ backend/aI-assistant-server/src/main/java/com/lucky/server/common/enums/SpeakingStageEnum.java
@@ -0,0 +1,57 @@
+package com.lucky.server.common.enums;
+
+import com.baomidou.mybatisplus.annotation.EnumValue;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.lucky.server.common.basic.BusinessException;
+import lombok.Getter;
+
+/**
+ * 口语学习阶段枚举
+ * @author shiningCloud2025
+ */
+public enum SpeakingStageEnum {
+
+    EN_PRIMARY("en_primary", "小学英语", "english"),
+    EN_JUNIOR("en_junior", "初中英语", "english"),
+    EN_SENIOR("en_senior", "高中英语", "english"),
+    EN_CET4("en_cet4", "大学英语四级", "english"),
+    EN_CET6("en_cet6", "大学英语六级", "english"),
+    EN_POSTGRADUATE("en_postgraduate", "考研英语", "english"),
+    EN_IELTS("en_ielts", "雅思", "english"),
+    EN_TOEFL("en_toefl", "托福", "english");
+
+    @EnumValue
+    private final String code;
+
+    @Getter
+    private final String desc;
+
+    @Getter
+    private final String languageCode;
+
+    SpeakingStageEnum(String code, String desc, String languageCode) {
+        this.code = code;
+        this.desc = desc;
+        this.languageCode = languageCode;
+    }
+
+    @JsonValue
+    public String getCode() {
+        return code;
+    }
+
+    public boolean belongsToLanguage(String languageCode) {
+        return this.languageCode.equals(languageCode);
+    }
+
+    @JsonCreator
+    public static SpeakingStageEnum fromCode(String code) {
+        for (SpeakingStageEnum e : values()) {
+            if (e.code.equals(code)) {
+                return e;
+            }
+        }
+        throw new BusinessException(ResultCodeEnum.PARAM_ERROR, "不支持的口语学习阶段: " + code);
+    }
+}
